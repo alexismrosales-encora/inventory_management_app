7688fb91248d182282316f69fae912f6a714810a
Jorge Marquez <jorge.marquez@Jorges-MacBook-Pro.local>
2025-03-06 02:25:45 -0600

    feat: new elements added to pagination

--- backend/src/main/java/com/breakabletoy/ima_backend/dto/PaginationRequestDTO.java
+++ backend/src/main/java/com/breakabletoy/ima_backend/dto/PaginationRequestDTO.java
@@ -1,18 +1,26 @@
 package com.breakabletoy.ima_backend.dto;
 
+import com.breakabletoy.ima_backend.enums.StockStatus;
+
 public class PaginationRequestDTO {
     private int page;
     private int size;
     private String sortBy;
     private String sortOrder;
+    private StockStatus stockStatus;
+    private String category;
+    private String search;
 
     public PaginationRequestDTO() {}
 
-    public PaginationRequestDTO(int page, int size, String sortBy, String sortOrder) {
+    public PaginationRequestDTO(int page, int size, String sortBy, String sortOrder, StockStatus stockStatus, String category, String search) {
         this.page = page;
         this.size = size;
         this.sortBy = sortBy;
         this.sortOrder = sortOrder;
+        this.stockStatus = stockStatus;
+        this.category = category;
+        this.search = search;
     }
 
     public int getPage() {
@@ -45,5 +53,29 @@ public class PaginationRequestDTO {
 
     public void setSortOrder(String sortOrder) {
         this.sortOrder = sortOrder;
+    }
+
+    public StockStatus getStockStatus() {
+        return stockStatus;
+    }
+
+    public void setStockStatus(StockStatus stockStatus) {
+        this.stockStatus = stockStatus;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getSearch() {
+        return search;
+    }
+
+    public void setSearch(String search) {
+        this.search = search;
     }
 }
