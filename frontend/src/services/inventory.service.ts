d035d7327e70ee1afd14c7076056e86481257b6e
Jorge Marquez <jorge.marquez@Jorges-MacBook-Pro.local>
2025-03-08 20:35:15 -0600

    feat: creation of new connections with endpoints

--- frontend/src/services/inventory.service.ts
+++ frontend/src/services/inventory.service.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-import { InventoryItem } from '../types/inventory'
+import { Filters, InventoryItem, PaginatedResponse, Pagination } from '../types/inventory'
+import { StockStatus } from '../utils/inventory.utils'
 
 const API_URL = import.meta.env.VITE_API_BASEURL + '/products'
 
@@ -10,23 +11,27 @@ const createInventoryItem = async (inventoryItem: InventoryItem): Promise<Invent
   return response.data
 }
 
-const getAllItems = async (): Promise<InventoryItem[]> => {
+const getAllItems = async (pagination: Pagination, filters: Filters): Promise<PaginatedResponse> => {
   // Here would be useful to use react context
-  const response = await axios.get<InventoryItem[]>(API_URL, {
+  const response = await axios.get<PaginatedResponse>(API_URL, {
     params: {
-      page: 1,
-      size: 10,
-      sortBy: "price",
-      sortOrder: "asc",
+      ...pagination,
+      ...filters,
+      stockStatus: filters.stockStatus !== null ? StockStatus[filters.stockStatus] : null,
     },
   });
   return response.data
 }
 
-const inventoryService = {
-  createInventoryItem,
-  getAllItems
+const getCategories = async (): Promise<string[]> => {
+  const response = await axios.get<string[]>(API_URL + '/categories')
+  return response.data
 }
 
+const inventoryService = {
+  createInventoryItem,
+  getAllItems,
+  getCategories
+}
 
 export default inventoryService
