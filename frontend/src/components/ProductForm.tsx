d744d889f7dd344e52b6535e38e194510e502c76
Jorge Marquez <jorge.marquez@Jorges-MacBook-Pro.local>
2025-03-10 22:12:33 -0600

    refactor: making product form reusable for creating and editing

--- frontend/src/components/ProductForm.tsx
+++ frontend/src/components/ProductForm.tsx
@@ -1,40 +1,60 @@
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import inventoryService from "../services/inventory.service"
 import { InventoryItem, Product } from "../types/inventory";
 import { StockStatus } from "../utils/inventory.utils";
+import { InventoryContext } from "../context/InventoryContext";
 
-const ProductForm = () => {
-  const [selectedName, setSelectedName] = useState("")
+interface ProductFormProps {
+  productToEdit?: InventoryItem | null; // Optional prop for editing
+  onClose: () => void; // Function to close the form
+}
+
+const ProductForm = ({ productToEdit, onClose }: ProductFormProps) => {
+  const context = useContext(InventoryContext)
+  if (!context) {
+    return null
+  }
+  const { shouldUpdateTable, setShouldUpdateTable } = context.triggerTableUpdateType
+
+  const [selectedName, setSelectedName] = useState(productToEdit?.product.name || "")
   const [categories, setCategories] = useState<string[]>([]);
-  const [selectedCategory, setSelectedCategory] = useState("")
-  const [selectedStock, setSelectedStock] = useState(10)
-  const [selectedUnitPrice, setSelectedUnitPrice] = useState(0)
-  const [selectedDate, setSelectedDate] = useState("")
+  const [selectedCategory, setSelectedCategory] = useState(productToEdit?.product.category || "")
+  const [selectedStock, setSelectedStock] = useState(productToEdit?.quantity || 10)
+  const [selectedUnitPrice, setSelectedUnitPrice] = useState(productToEdit?.product.price || 0)
+  const [selectedDate, setSelectedDate] = useState<Date | null>(productToEdit?.product.expiryDate ? productToEdit.product.expiryDate : null)
 
   useEffect(() => {
     inventoryService.getCategories().then((response) => {
       setCategories(response)
     })
-  }, [])
+  }, [shouldUpdateTable])
 
   const handleSumbit = (e: React.FormEvent) => {
     e.preventDefault()
     const product: Product = {
-      id: 0,
+      id: productToEdit?.id || 0,
       name: selectedName,
       category: selectedCategory,
       price: selectedUnitPrice,
-      expiryDate: new Date(selectedDate),
+      expiryDate: selectedDate ? new Date(selectedDate) : null,
       dateCreated: new Date(),
       dateUpdated: new Date()
     }
     const inventory: InventoryItem = {
-      id: 0,
+      id: productToEdit?.id || 0,
       product: product,
       quantity: selectedStock,
       stockStatus: StockStatus.IN_STOCK
     }
-    inventoryService.createInventoryItem(inventory)
+
+    if (productToEdit) {
+      inventoryService.updateInventoryItem(productToEdit.id)
+    } else {
+      inventoryService.createInventoryItem(inventory)
+    }
+    // trigger table to update it
+    setShouldUpdateTable(prev => !prev)
+    onClose()
   }
 
   return <div>
@@ -51,7 +71,7 @@ const ProductForm = () => {
           ))}
         </select>
         <button type="button"> New category </button>
-        <input type="text" name="newCategory" value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)} />
+        <input type="text" name="newCategory" value={selectedCategory.toString()} onChange={(e) => setSelectedCategory(e.target.value)} />
       </label>
       <label>
         Stock
@@ -63,7 +83,7 @@ const ProductForm = () => {
       </label>
       <label>
         Expiration Date
-        <input type="date" name="expirationDate" value={selectedDate} onChange={(e) => setSelectedDate(e.target.value)} />
+        <input type="date" name="expirationDate" value={selectedDate ? selectedDate.toString() : "N/A"} onChange={(e) => setSelectedDate(e.target.value ? new Date(e.target.value) : null)} />
       </label>
       <button type="submit" onClick={handleSumbit}>Save</button>
       <button type="button">Cancel</button>
