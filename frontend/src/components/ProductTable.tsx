09fa40f773bb420f538b50d7f2098d5323a06a20
Jorge Marquez <jorge.marquez@Jorges-MacBook-Pro.local>
2025-03-12 23:11:35 -0600

    chore: style created

--- frontend/src/components/ProductTable.tsx
+++ frontend/src/components/ProductTable.tsx
@@ -1,12 +1,16 @@
 import { ProductRowHeader, ProductRows } from './ProductRow'
 const ProductTable = () => {
-  return <table>
-    <thead>
-      <ProductRowHeader />
-    </thead>
-    <tbody>
-      <ProductRows />
-    </tbody>
-  </table>
+  return <div className="relative overflow-x-auto">
+    <div className="text-lg md:w-7/12 sm:w-full">
+      <table className="table-auto border-collapse w-full text-gray-900">
+        <thead className="border-b-2 border-primary-500">
+          <ProductRowHeader />
+        </thead>
+        <tbody>
+          <ProductRows />
+        </tbody>
+      </table>
+    </div>
+  </div>
 }
 export default ProductTable
