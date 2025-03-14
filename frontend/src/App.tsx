cc2eab7448d503de981e8475b5f5cba098cfced7
Jorge Marquez <jorge.marquez@Jorges-MacBook-Pro.local>
2025-03-13 19:01:39 -0600

    chore: style changed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import InventoryPage from './pages/InventoryPage'
 
 function App() {
   return (
-    <div className="font-inter md:text-lg text-xl p-6">
+    <div className="font-inter md:text-lg text-xl p-6 md:px-[12rem] container mx-auto md:mx-auto">
       <InventoryPage />
     </div>
   )
