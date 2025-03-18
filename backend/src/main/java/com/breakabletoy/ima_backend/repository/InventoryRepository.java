e123ad4cc93453caeee1adba6596c3d3543b2607
Alexis M.Rosales <alexismarquezrosales@gmail.com>
2025-03-17 19:48:56 -0600

    chore: removed unnecesary functions

--- backend/src/main/java/com/breakabletoy/ima_backend/repository/InventoryRepository.java
+++ backend/src/main/java/com/breakabletoy/ima_backend/repository/InventoryRepository.java
@@ -1,5 +1,7 @@
 package com.breakabletoy.ima_backend.repository;
 
+import com.breakabletoy.ima_backend.dto.CategoryMetric;
+import com.breakabletoy.ima_backend.dto.MetricsDTO;
 import com.breakabletoy.ima_backend.dto.PaginationRequestDTO;
 import com.breakabletoy.ima_backend.entity.Inventory;
 
@@ -9,9 +11,11 @@ import java.util.Optional;
 public interface InventoryRepository {
     Optional<Inventory> findById(Long id);
     Inventory save(Inventory inventory);
-    Inventory update(Long id, Inventory inventory);
-    Inventory markOutOfStockById(Long id);
-    Inventory markInStockById(Long id);
+    Inventory update(Inventory inventory);
     void deleteById(Long id);
+    Inventory getById(Long id);
     List<Inventory> findAll(PaginationRequestDTO paginationRequestDTO);
+    List<Inventory> findAllWithoutPagination();
+    List<String> findAllCategories();
+    long totalItems();
 }
